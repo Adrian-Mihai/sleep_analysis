54e9e373d130809e5b3c0b37bce5d66b6dd41132
Ardelean Adrian <ardelean.adrian.mihai@gmail.com>
2021-06-06 14:50:08 +0300

    restful for users

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, :first_name, :last_name, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, confirmation: true, length: { minimum: 8 }, if: -> { password.present? }
+  validates :password_confirmation, presence: true, if: -> { password.present? }
+end
